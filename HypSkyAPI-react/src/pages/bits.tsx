761c260ed734db16c7960b560daf13df5576642f
ovdp <othniel@pol-it.nl>
2024-05-30 23:13:16 +0200

    api call system 0.1

--- HypSkyAPI-react/src/pages/bits.tsx
+++ HypSkyAPI-react/src/pages/bits.tsx
@@ -1,6 +1,7 @@
 import API from '../services/api/api';
 
 export default function bits() {
+
   return (
     <div className='p-3 pb-md-4 mx-auto text-center'>
       <h1>
@@ -8,7 +9,7 @@ export default function bits() {
       </h1>
       <sub>
         <p>
-          <API />
+          <API/>
         </p>
       </sub>
     </div>
--- HypSkyAPI-react/src/services/api/api.tsx
+++ HypSkyAPI-react/src/services/api/api.tsx
@@ -1,10 +1,26 @@
-import ApiKey from "../../../apikey.json";
+import { useEffect, useState } from "react";
+const url = "https://api.hypixel.net/skyblock/auctions";
 
-const key = ApiKey.key;
+export default function Api() {
+  const [aap, zetAap] = useState([]);
 
-export default function api() {
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(url);
+      response.json().then(json => {
+        for (let i = 0; i < json.totalPages; i++) {
+          for (let j = 0; j < json.auctions.length; j++) {
+            zetAap(prevApiResponse => [...prevApiResponse, json]);
+          }
+        }
+      });
+    }
+    fetchData();
+  }, [aap]);
+  console.log(aap);
   return (
-    <div>{key}</div>
+    <div>
+      <div>API Response: {JSON.stringify(aap)}</div>
+    </div>
   )
 }
-
