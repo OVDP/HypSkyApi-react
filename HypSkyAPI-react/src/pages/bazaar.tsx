3f00e23448f02f5206cc3be6f81cfbb2112ceb80
ovdp <othniel@pol-it.nl>
2024-06-02 23:30:01 +0200

    setup bazaar v0.1

--- HypSkyAPI-react/src/pages/bazaar.tsx
+++ HypSkyAPI-react/src/pages/bazaar.tsx
@@ -1,9 +1,11 @@
+import Api from "../services/api/bz_api"
+
 export default function bazaar() {
   return (
+    <>
     <div className='p-3 pb-md-4 mx-auto text-center'>
-      <h1>
-        WIP
-      </h1>
+      <Api/>
     </div>
+    </>
   )
 }
---
+++ HypSkyAPI-react/src/services/api/bz_api.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+const url = "https://api.hypixel.net/skyblock/bazaar";
+
+export default function Api() {
+    const [bazaar, setBazaar] = useState(0);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(url);
+                response.json().then(json => {
+                    setBazaar(json);
+                });
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+        fetchData();
+    }, []);
+      
+  return (
+    <div>{JSON.stringify(bazaar)}</div>
+  )
+}
